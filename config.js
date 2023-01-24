ed5e0df3f75f01f1412e68d03795cabaacb07a3d
1x000 <huaweilaomke402@gmail.com>
2023-01-24 17:04:00 +0700

    加特技duang

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'LEl_FENG网站监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1719682-42b661a8edabd10744c06e1c',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://www.xpdbk.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/1x000'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.xpdbk.com/'
+    },
+  ],
+};
